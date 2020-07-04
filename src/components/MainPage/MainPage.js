dd608605f7e11e11f71148603ccfd274c3b5d2f2
4workacc <58693140+4workacc@users.noreply.github.com>
2020-07-04 18:53:40 +0300

    Update MainPage.js

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -4,6 +4,45 @@ import "./MainPage.scss";
 class MainPage extends React.Component {
     constructor ( props ) {
         super ( props );
+        this.state = {
+            userId : -1,
+            
+            menuSet : [
+                {
+                    "title" : "Menu0",
+                    "className" : "MainPage_MainMenu_0"
+                },
+                {
+                    "title" : "Menu1",
+                    "className" : "MainPage_MainMenu_1"
+                },
+                {
+                    "title" : "Menu2",
+                    "className" : "MainPage_MainMenu_2"
+                },
+                {
+                    "title" : "Menu3",
+                    "className" : "MainPage_MainMenu_3"
+                },
+                {
+                    "title" : "Menu4",
+                    "className" : "MainPage_MainMenu_4"
+                },
+                {
+                    "title" : "Menu5",
+                    "className" : "MainPage_MainMenu_5"
+                },
+                {
+                    "title" : "Menu6",
+                    "className" : "MainPage_MainMenu_6"
+                }
+            ],
+            curMenuSet : [],
+            curStartMenuIndex : 0
+        }
+    }
+    showCurMenuSet(){
+        this.state
     }
     render() {
         return (
