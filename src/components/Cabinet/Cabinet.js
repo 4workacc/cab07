d710ff5c015d85fca7306b609e532432fa313df8
Eugen <4workacc@gmail.com>
2020-08-05 20:48:33 +0300

    kl

---
+++ src/components/Cabinet/Cabinet.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import 'Cabenet.css';
+
+const Cabinet = () => {
+    return(
+        <div className = 'Cabinet'>
+            cabinet
+        </div>
+    )
+}
+
+export default Cabinet;
