02baa6e6c2f173fb53fc63d7e6205dbee760e01b
4workacc <58693140+4workacc@users.noreply.github.com>
2020-09-23 13:41:43 +0300

    CLEATR

--- src/redux/actions/AShowPage.js
+++
@@ -1,7 +0,0 @@
-let AShowPage = ( pageName ) => {
-    return {
-        type : "SHOW_PAGE",
-        page : pageName
-    }
-}
-export default AShowPage;
--- src/redux/actions/AShowTest.js
+++
@@ -1,8 +0,0 @@
-let AShowTest = ( pageName, taskId ) => {
-    return {
-        type : "SHOW_TEST",
-        page : pageName,
-        curUserTask : taskId
-    }
-}
-export default AShowTest;
